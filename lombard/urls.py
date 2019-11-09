51a76e13a6b8b2fb4c0edfa7293a71b0e8b7d38c
zfazylz <faza.zheten123@gmail.com>
2019-11-10 00:10:26 +0600

    statics

--- lombard/urls.py
+++ lombard/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    path('admin/', admin.site.urls),
-]
+                  path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
